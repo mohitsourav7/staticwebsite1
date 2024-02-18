d38fee0f8b2c8e46f465877dfaeec3c3fcb40ba3
mohitsourav7 <97936963+mohitsourav7@users.noreply.github.com>
2024-02-18 15:33:53 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// $(document).ready(functio(){
+//         // Initialize the carousel
+//         var myCarousel = document.getElementById('carouselExampleIndicators');
+//         var carousel = new bootstrap.Carousel(myCarousel, {
+//           interval: 2000, // Set the interval (in milliseconds) for automatic sliding
+//           wrap: true // Enable continuous loop of carousel items
+//         });
+// })
